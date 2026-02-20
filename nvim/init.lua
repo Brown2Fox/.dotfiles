a28cb86b83cfeb37c11d4a70b3219d522cf32b1c
Andrey Akimov <a.a.m.incorp@yandex.ru>
2026-02-20 08:16:27 +0300

    update nvim init

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,14 +1,19 @@
 local k = vim.keycode
 
-vim.g.mapleader = k '<F15>'
-vim.g.maplocalleader = k '<F15>'
+vim.g.mapleader = k("<F15>")
+vim.g.maplocalleader = k("<F15>")
 
 -- Set to true if you have a Nerd Font installed and selected in the terminal
 vim.g.have_nerd_font = true
 vim.o.guifont = "Lilex Nerd Font:h14"
 
--- vim.o.clipboard = "unnamed,unnamedplus"
+vim.o.clipboard = "unnamed,unnamedplus"
 
+if vim.g.neovide then
+	-- Put anything you want to happen only in Neovide here
+	vim.g.neovide_opacity = 0.9
+	vim.g.neovide_normal_opacity = 0.8
+end
 -- 	name = "Clipboard",
 -- 	copy = {
 -- 		["+"] = "clip.exe",
@@ -34,4 +39,4 @@ vim.o.guifont = "Lilex Nerd Font:h14"
 -- require 'lazy-plugins'
 
 -- bootstrap lazy.nvim, LazyVim and your plugins
-require 'config.lazy'
+require("config.lazy")
