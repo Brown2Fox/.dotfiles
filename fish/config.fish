c0dd87fa66c5f63ac39154a61770690873a9a386
Andrey Akimov <a.a.m.incorp@yandex.ru>
2026-02-20 08:15:18 +0300

    update fish config

--- fish/config.fish
+++ fish/config.fish
@@ -2,3 +2,14 @@ source /usr/share/cachyos-fish-config/cachyos-config.fish
 
 set -g fish_prompt_pwd_dir_length 0
 set -g fish_key_bindings fish_vi_key_bindings
+
+set FZF_DEFAULT_OPTS "--layout=reverse --border=bold --border=rounded --margin=3% --color=dark"
+
+set EDITOR "nvim"
+set VISUAL "neovide"
+
+alias cls "clear"
+alias nvide "neovide"
+
+# Fzf integration:
+fzf --fish | source
