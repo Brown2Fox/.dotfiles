cc12bb1fa7bccdb9a324f8fd8662d795b4b27456
Andrey Akimov <a.a.m.incorp@yandex.ru>
2026-02-28 09:14:44 +0300

    update fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,5 +1,7 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
+source ~/.config/fish/themes/tokyonight_night.fish
+
 set -g fish_prompt_pwd_dir_length 0
 set -g fish_key_bindings fish_vi_key_bindings
 
