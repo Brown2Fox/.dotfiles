a7923b2f51255150bb045362438102837a933b5c
Andrey Akimov <a.a.m.incorp@yandex.ru>
2026-03-21 00:20:20 +0300

    update fish

--- fish/.config/fish/config.fish
+++ fish/.config/fish/config.fish
@@ -1,6 +1,6 @@
 source /usr/share/cachyos-fish-config/cachyos-config.fish
 
-source ~/.config/fish/themes/tokyonight_night.fish
+fish_config theme choose "Rosé Pine Moon Auto"
 
 set -g fish_prompt_pwd_dir_length 0
 set -g fish_key_bindings fish_vi_key_bindings
@@ -17,6 +17,16 @@ set -gx fish_tmux_default_session_name "default"
 
 alias cls "clear"
 alias nvide "neovide"
+alias vv "NVIM_APPNAME=nvim-simp nvim"
+
+
+function fish_user_key_bindings
+  # bind yy fish_clipboard_copy
+  bind y fish_clipboard_copy
+  bind p fish_clipboard_paste
+  bind -s --preset -M visual -m default y "fish_clipboard_copy; commandline -f end-selection repaint-mode"
+  bind \$ 'commandline -f end-of-line repaint-mode'
+end
 
 # Fzf integration:
 fzf --fish | source
